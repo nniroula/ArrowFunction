d66418b146a442020fe91c4c94ac1ef0bb5f1924
Nabin <nkbhaie2017@gmail.com>
2021-02-05 23:11:47 -0700

    add arrow function refactor

--- arrowFunction.js
+++ arrowFunction.js
@@ -13,3 +13,20 @@ Refactor the above code to use two arrow functions. Turn it into a one-liner.
 
 /* Write an ES2015 Version */
 const doubleArr = arr =>  arr.map(val => val*2); 
+
+/*
+Refactor the following function to use arrow functions:
+Replace ALL functions with arrow functions:
+
+function squareAndFindEvens(numbers){
+  var squares = numbers.map(function(num){
+    return num ** 2;
+  });
+  var evens = squares.filter(function(square){
+    return square % 2 === 0;
+  });
+  return evens;
+}
+*/
+
+const refactorSquareAndFindEvens = numbers => numbers.map(num => num**2).filter(square => square % 2 === 0)
